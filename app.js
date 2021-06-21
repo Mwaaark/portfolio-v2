08168af3d513d1e7777ba53d99319278f19ddfb4
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-06-22 00:07:27 +0800

    add findmovies project description and setup formspree

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
+const completedTab = document.getElementById("pills-completed-tab");
+const inProgressTab = document.getElementById("pills-in-progress-tab");
+
+completedTab.addEventListener("click", function () {
+  setTimeout(function () {
+    AOS.refreshHard();
+  }, 500);
+});
+
+inProgressTab.addEventListener("click", function () {
+  setTimeout(function () {
+    AOS.refreshHard();
+  }, 500);
+});
